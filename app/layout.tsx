5ba1bafd1be5a43a2b117ebc3ef634c8c74c937e
Nassira Properties <dev@nassiraproperties.com>
2025-11-20 20:40:36 +0400

    Remove duplicate pixel, keep only Nassira Properties pixel (1345773383529169)

--- app/layout.tsx
+++ app/layout.tsx
@@ -136,38 +136,11 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
         />
-        {/* Meta Pixel Code - New Account (1251459406816726) */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1251459406816726');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1251459406816726&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
-        {/* End Meta Pixel Code */}
       </head>
       <body className={`font-sans antialiased`}>
-        {/* Meta Pixel - Old Account */}
+        {/* Meta Pixel - Nassira Properties Account */}
         <Script
-          id="facebook-pixel-old"
+          id="facebook-pixel"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
@@ -190,7 +163,7 @@ export default function RootLayout({
             width="1"
             style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=1345773383529169&ev=PageView&noscript=1"
-            alt="facebook pixel old"
+            alt=""
           />
         </noscript>
 
